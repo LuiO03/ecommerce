552a58a41f18e6c440d1749d0f430ab5fb08d87a
luis <lui.fenixand.1997@gmail.com>
2026-04-29 11:58:52 -0500

    ....

--- app/Http/Controllers/Admin/FamilyController.php
+++ app/Http/Controllers/Admin/FamilyController.php
@@ -104,7 +104,6 @@ class FamilyController extends Controller
         // Generar y descargar PDF
         return Pdf::view('admin.export.families-pdf', compact('families'))
             ->withBrowsershot(function ($browser) {
-                $browser->setChromePath(env('CHROME_PATH'));
                 $browser->noSandbox();
             })
             ->format('a4')
